3818f6ff41ecf9b069b61ff8c0c96a9c72817e0e
nganhkhoa <mail.nganhkhoa@gmail.com>
2022-03-03 05:26:26 +0000

    format code

--- chain-backend/pallets/template/src/lib.rs
+++ chain-backend/pallets/template/src/lib.rs
@@ -2,108 +2,107 @@
 
 pub use pallet::*;
 
-
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-    use sp_std::vec::Vec;
+	use sp_std::vec::Vec;
 
-    #[pallet::config]
-    pub trait Config: frame_system::Config {
-        /// Because this pallet emits events, it depends on the runtime's definition of an event.
-        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-    }
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
 
-    #[pallet::event]
-    #[pallet::generate_deposit(pub(super) fn deposit_event)]
-    pub enum Event<T: Config> {
-        /// Event emitted when a proof has been claimed. [who, claim]
-        ClaimCreated(T::AccountId, Vec<u8>),
-        /// Event emitted when a claim is revoked by the owner. [who, claim]
-        ClaimRevoked(T::AccountId, Vec<u8>),
-    }
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Event emitted when a proof has been claimed. [who, claim]
+		ClaimCreated(T::AccountId, Vec<u8>),
+		/// Event emitted when a claim is revoked by the owner. [who, claim]
+		ClaimRevoked(T::AccountId, Vec<u8>),
+	}
 
-    #[pallet::error]
-    pub enum Error<T> {
-        /// The proof has already been claimed.
-        ProofAlreadyClaimed,
-        /// The proof does not exist, so it cannot be revoked.
-        NoSuchProof,
-        /// The proof is claimed by another account, so caller can't revoke it.
-        NotProofOwner,
-    }
+	#[pallet::error]
+	pub enum Error<T> {
+		/// The proof has already been claimed.
+		ProofAlreadyClaimed,
+		/// The proof does not exist, so it cannot be revoked.
+		NoSuchProof,
+		/// The proof is claimed by another account, so caller can't revoke it.
+		NotProofOwner,
+	}
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
-    #[pallet::without_storage_info]
+	#[pallet::without_storage_info]
 	pub struct Pallet<T>(_);
 
-    #[pallet::storage]
-    pub(super) type Proofs<T: Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, Option<(T::AccountId, T::BlockNumber)>, ValueQuery>;
+	#[pallet::storage]
+	pub(super) type Proofs<T: Config> = StorageMap<
+		_,
+		Blake2_128Concat,
+		Vec<u8>,
+		Option<(T::AccountId, T::BlockNumber)>,
+		ValueQuery,
+	>;
 
 	#[pallet::hooks]
-    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-    // Dispatchable functions allow users to interact with the pallet and invoke state changes.
-    // These functions materialize as "extrinsics", which are often compared to transactions.
-    // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
-    #[pallet::call]
-    impl<T: Config> Pallet<T> {
-        #[pallet::weight(1_000)]
-        pub fn create_claim(
-            origin: OriginFor<T>,
-            proof: Vec<u8>,
-        ) -> DispatchResult {
-            // Check that the extrinsic was signed and get the signer.
-            // This function will return an error if the extrinsic is not signed.
-            // https://docs.substrate.io/v3/runtime/origins
-            let sender = ensure_signed(origin)?;
+	// Dispatchable functions allow users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(1_000)]
+		pub fn create_claim(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let sender = ensure_signed(origin)?;
 
-            // Verify that the specified proof has not already been claimed.
-            ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
+			// Verify that the specified proof has not already been claimed.
+			ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
 
-            // Get the block number from the FRAME System pallet.
-            let current_block = <frame_system::Pallet<T>>::block_number();
+			// Get the block number from the FRAME System pallet.
+			let current_block = <frame_system::Pallet<T>>::block_number();
 
-            // Store the proof with the sender and block number.
-            let claim = Some((sender.clone(), current_block));
-            log::info!("insert claim: {:?}", claim);
-            Proofs::<T>::insert(&proof, claim);
+			// Store the proof with the sender and block number.
+			let claim = Some((sender.clone(), current_block));
+			log::info!("insert claim: {:?}", claim);
+			Proofs::<T>::insert(&proof, claim);
 
-            // Emit an event that the claim was created.
-            Self::deposit_event(Event::ClaimCreated(sender, proof));
+			// Emit an event that the claim was created.
+			Self::deposit_event(Event::ClaimCreated(sender, proof));
 
-            Ok(())
-        }
+			Ok(())
+		}
 
-        #[pallet::weight(10_000)]
-        pub fn revoke_claim(
-            origin: OriginFor<T>,
-            proof: Vec<u8>,
-        ) -> DispatchResult {
-            // Check that the extrinsic was signed and get the signer.
-            // This function will return an error if the extrinsic is not signed.
-            // https://docs.substrate.io/v3/runtime/origins
-            let sender = ensure_signed(origin)?;
+		#[pallet::weight(10_000)]
+		pub fn revoke_claim(origin: OriginFor<T>, proof: Vec<u8>) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/v3/runtime/origins
+			let sender = ensure_signed(origin)?;
 
-            // Get owner of the claim.
-            let claim = Proofs::<T>::get(&proof);
-            log::info!("get claim: {:?}", claim);
-            ensure!(claim.is_some(), Error::<T>::NoSuchProof);
+			// Get owner of the claim.
+			let claim = Proofs::<T>::get(&proof);
+			log::info!("get claim: {:?}", claim);
+			ensure!(claim.is_some(), Error::<T>::NoSuchProof);
 
-            let claim = claim.unwrap();
-            let (owner, _) = claim;
+			let claim = claim.unwrap();
+			let (owner, _) = claim;
 
-            // Verify that sender of the current call is the claim owner.
-            ensure!(owner == sender, Error::<T>::NotProofOwner);
+			// Verify that sender of the current call is the claim owner.
+			ensure!(owner == sender, Error::<T>::NotProofOwner);
 
-            // Remove claim from storage.
-            Proofs::<T>::remove(&proof);
+			// Remove claim from storage.
+			Proofs::<T>::remove(&proof);
 
-            // Emit an event that the claim was erased.
-            Self::deposit_event(Event::ClaimRevoked(sender, proof));
-            Ok(())
-        }
-    }
+			// Emit an event that the claim was erased.
+			Self::deposit_event(Event::ClaimRevoked(sender, proof));
+			Ok(())
+		}
+	}
 }
